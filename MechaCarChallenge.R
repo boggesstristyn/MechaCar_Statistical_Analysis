4890a1932b5e42e4ed9e1bf3d5056211add83cf2
boggesstristyn <boggess.tristyn@gmail.com>
2022-08-06 11:10:09 -0700

    Deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,7 +1,6 @@
 library(dplyr)
 
 #Deliverable 1
-
 #read in csv
 MechaMPG <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 head(MechaMPG)
@@ -12,9 +11,23 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 
 
 #Deliverable 2
-
 #read in csv
 Suspension_Coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
 #statistical summary
 total_summary <- Suspension_Coil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
 lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
+
+
+#Deliverable 3
+#select random 75 from total population
+sample <- Suspension_Coil %>% sample_n(75)
+#compare means of population with population mean
+t.test(Suspension_Coil$PSI, mu = 1500)
+#sample of population compared with population mean of 1500 PSI
+t.test(sample$PSI, mu = 1500)
+# T-Test for Lot 1
+t.test(subset(Suspension_Coil$PSI,Suspension_Coil$Manufacturing_Lot == "Lot1"),mu=1500)
+#T-Test for Lot 2
+t.test(subset(Suspension_Coil$PSI,Suspension_Coil$Manufacturing_Lot == "Lot2"),mu=1500)
+# T-Test for Lot 3
+t.test(subset(Suspension_Coil$PSI,Suspension_Coil$Manufacturing_Lot == "Lot3"),mu=1500)
