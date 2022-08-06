c7a22369c598f2a7e2abafa757ae11ee501a2295
boggesstristyn <boggess.tristyn@gmail.com>
2022-08-06 10:45:40 -0700

    Deliverable 2 - Technical analysis suspension coil

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -9,3 +9,12 @@ head(MechaMPG)
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, MechaMPG)
 #summarize
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, MechaMPG))
+
+
+#Deliverable 2
+
+#read in csv
+Suspension_Coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+#statistical summary
+total_summary <- Suspension_Coil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
